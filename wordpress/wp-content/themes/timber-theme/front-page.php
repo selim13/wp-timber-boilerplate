a3605c71d80c9ae2c2d84909a7d4e3f75e760c28
selim13 <selim013@gmail.com>
2021-06-30 10:24:43 +0300

    add front page

---
+++ wordpress/wp-content/themes/timber-theme/front-page.php
@@ -0,0 +1,7 @@
+<?php
+
+use Timber\Timber;
+
+$context = Timber::context();
+$context['post'] = Timber::get_post();
+Timber::render('front-page.twig', $context);
--- wordpress/wp-content/themes/timber-theme/index.php
+++ wordpress/wp-content/themes/timber-theme/index.php
@@ -4,8 +4,4 @@ use Timber\Timber;
 
 $context = Timber::context();
 $context['posts'] = Timber::get_post();
-$templates = ['index.twig'];
-if (is_home()) {
-    $templates = ['front-page.twig', 'index.twig'];
-}
-Timber::render($templates, $context);
+Timber::render('index.twig', $context);
